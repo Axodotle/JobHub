3b6b004e7489dc178507e9fb7e8c65693dce8416
Jeremy Kay <jeremyafterj@gmail.com>
2024-06-18 15:48:08 -0700

    more stuff

--- client/test/App.test.jsx
+++ client/test/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
+// import React from 'react';
 import App from '../src/App.jsx';
 import renderer from 'react-test-renderer';
 
