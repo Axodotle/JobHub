342944e1f4b28aee94be54d0af62c6454b9bb101
artmontinski <artmontinski@gmail.com>
2024-06-18 18:25:34 -0700

    Fixed error handling, cleaned up, removed extras

--- server/server.js
+++ server/server.js
@@ -1,25 +1,26 @@
 const express = require('express');
-// const { restart } = require('nodemon');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const routerSignup = require('./routes/users'); //Change variable label to userRouter?
 const applicationsRouter = require('./routes/applications');
-
-const app = express();
+const routerSignup = require('./routes/users'); //Change variable label to userRouter?
 const path = require('path');
+const app = express();
 
-//
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const indexPath = path.join(__dirname, '../client/public/index.html');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
-// statically serve everything in the build folder on the route '/build'
-// app.use('/build', express.static(path.join(__dirname, '../build')));
-// serve index.html on the route '/'
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'), function (err) {
+app.get('/', (req, res) => {
+  res.sendFile(indexPath, (err) => {
     if (err) {
-      res.status(500).send(err);
+      console.error(`Error sending index.html: ${err}`);
+      res.status(500).send({
+        log: `Error sending index.html: ${err}`,
+        status: 'error',
+        code: 500,
+        message: 'Something went wrong while serving the request',
+      });
     }
   });
 });
@@ -29,16 +30,18 @@ app.use('/applications', applicationsRouter);
 
 // Global error handler:
 app.use((err, req, res, next) => {
+  // Default error structure
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
     status: 500,
-    message: { err: 'An error occurred' },
+    message: 'An error occurred',
   };
   const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+  // console.error(errorObj.message);
+  return res.status(errorObj.status).json({ error: errorObj.message });
 });
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
-}); //listens on port 3000 -> http://localhost:3000/
+});
+
+module.exports = app;
