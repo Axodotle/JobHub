382fdc90a6cd25c7f364e391268ce241ece18b44
Jeremy Kay <jeremyafterj@gmail.com>
2024-06-20 11:46:40 -0700

    reformat Jest tests to accomodate multiple tests

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -2,22 +2,49 @@
  * @jest-environment jsdom
  */
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { store } from './redux/store.jsx';
+import { Provider } from 'react-redux';
 
-test('loads and displays greeting', async () => {
-  // ARRANGE
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+describe('testing homepage', () => {
+  //see https://react.dev/reference/react/act
+  // https://testing-library.com/docs/queries/about/
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles
 
-  // ACT
-  await screen.findByRole('heading');
+  beforeAll(async () => {
+    // ARRANGE
+    const container = document.createElement('div');
+    act(() => {
+      document.body.appendChild(container);
+      const root = createRoot(container);
 
-  // ASSERT
-  expect(screen.getByRole('heading')).toHaveTextContent('JobHub');
+      root.render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      );
+    });
+  });
+
+  it('loads and displays main heading', async () => {
+    // ACT
+    await screen.findByRole('heading');
+    // ASSERT
+    expect(screen.getByRole('heading')).toHaveTextContent('JobHub');
+  });
+
+  //should probably be in login.test.jsx
+  it('loads and displays login form correctly', async () => {
+    // ACT
+    await screen.findByRole('form');
+    // ASSERT
+    expect(screen.getByRole('form')).toHaveTextContent('username');
+    expect(screen.getByRole('form')).toHaveTextContent('password');
+  });
 });
